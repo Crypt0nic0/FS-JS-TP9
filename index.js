19ecb95ba8d6fa559089bbc576b4cd58807d90b4
Crypt0nic0 <n.greneche45@gmail.com>
2023-05-12 21:31:39 +0200

    updated

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
-// 1 - Tester le lien de l'API dans le navigateur (https://restcountries.com/v3.1/all)
+let countries = [];
+const countryContainer = document.querySelector(".countries-container");
 
-// 2 - Créer une fonction pour "fetcher" les données, afficher les données dans la console.
+async function fetchCountries() {
+    await fetch('https://restcountries.com/v3.1/all')
+    .then((res) => res.json())
+    .then((data) => (countries = data));
 
-// 3 - Passer les données à une variable
+    countriesDisplay("", rangeValue.textContent);
+}
 
-// 4 - Créer une fonction d'affichage, et paramétrer l'affichage des cartes de chaque pays grace à la méthode MAP
+function countriesDisplay(search, nbAff){
+    countryContainer.innerHTML = countries
+    .map((country) => {
+        const countryNameUp = country.name.common.toUpperCase();
+        if(countryNameUp.includes(search))
+        {
+            return `
+                <img src=${country.flags.png} alt=${country.flags.alt}>
+                <h2>${country.name.common}</h2>
+                <h3>${country.capital}</h3>
+                <p>${country.population}</p>
+            `
+        }
+    })
+    .join("");
+}
 
-// 5 - Récupérer ce qui est tapé dans l'input et filtrer (avant le map) les données
-coutry.name.includes(inputSearch.value);
+fetchCountries();
+
+
+
+inputSearch.addEventListener("keyup", (e) => {
+    countriesDisplay(e.target.value.toUpperCase(), rangeValue.textContent);
+});
+
+inputRange.addEventListener("input", (e) => {
+    rangeValue.textContent = e.target.value;
+    countriesDisplay(inputSearch.value.toUpperCase(), rangeValue.textContent);
+});
+
+// 4 - Créer une fonction d'affichage, 
+//et paramétrer l'affichage des cartes de chaque pays grace à la méthode MAP
 
 // 6 - Avec la méthode Slice gérer le nombre de pays affichés (inputRange.value)
 
